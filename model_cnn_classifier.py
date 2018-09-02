6057238730fb7a9e16f91d948a9322f0b8143d34
caffeine110 <gaurav.developer.audio@gmail.com>
2018-09-02 10:21:30 +0530

    next update

--- model_cnn_classifier.py
+++ model_cnn_classifier.py
@@ -4,13 +4,13 @@
 Created on Tue Aug 21 12:06:22 2018
 
 @author : Gaurav Gahukar
-        : Caffeine110
 
-Aim : Convulational Neural Network 
+
+Aim : Convulational Neural Network to distinguish between two animals
 
 """
 
-
+#Phase - 1:  Import
 #importing all the required librariees
 from keras.models import Sequential
 from keras.layers import Convolution2D
@@ -18,55 +18,61 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+#Phase - 2: Building Model
 #Initialise the CNN
-cnn_classifier = Sequential()
+model_cnn_classifier = Sequential()
 
-#Convolutional step - 1
-cnn_classifier.add(Convolution2D(32,3,3,  input_shape = (64,64,3), activation= 'relu'))
 
-#Pooling step - 2
-cnn_classifier.add(MaxPooling2D(pool_size = (2,2)))
+#Convolutional step
+model_cnn_classifier.add(Convolution2D(32,3,3,  input_shape = (64,64,3), activation= 'relu'))
 
-#Flatten the step -3
-cnn_classifier.add(Flatten())
+#Pooling step
+model_cnn_classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+#Flatten the step
+model_cnn_classifier.add(Flatten())
  
-#Full connection -4
-cnn_classifier.add(Dense(output_dim = 128, activation= 'relu'))
-
-# output layer 
-#cnn_classifier.add(Dense(   ))
-cnn_classifier.add(Dense(output_dim = 1, activation= 'sigmoid'))
+#Full connection
+model_cnn_classifier.add(Dense(output_dim = 128, activation= 'relu'))
 
 
-#compiling the CNN
-cnn_classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+#Phase - (middle): Output layer 
+#model_cnn_classifier.add(Dense(   ))
+model_cnn_classifier.add(Dense(output_dim = 1, activation= 'sigmoid'))
 
 
-#stage 2 Fitting cnn to ithe image
-#from keras 
+#Phase - 3 :  compiling the CNN
+model_cnn_classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+#Phase - 4 : Fitting CNN to the image
+#Importing 
 from keras.preprocessing.image import ImageDataGenerator
 
+# Train Datagen
 train_datagen = ImageDataGenerator(
         rescale = 1./255,
         shear_range=0.2,
         horizontal_flip=True )
-
+#Test Daragen
 test_datagen = ImageDataGenerator(rescale=1./255)
 
+#Training Set
 training_set= train_datagen.flow_from_directory(
         'dataset/training_set',
         target_size = (64, 64),
         batch_size = 32,
         class_mode='binary')
 
-
+#test Set
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
         target_size = (64,64),
         batch_size = 32,
         class_mode='binary')
 
-cnn_classifier.fit_generator(training_set,
+#Final Step
+model_cnn_classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
                          epochs = 25,
                          validation_data = test_set,
